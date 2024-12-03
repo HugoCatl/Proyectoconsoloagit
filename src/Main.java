9a3ffdb7fa792fae6da9fc07c8b2cacc17f1b9ec
HugoCatl <hugocaiba@gmail.com>
2024-12-03 22:49:15 +0100

    metodos multiplicar y dividir

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola, este es un proyecto de consola.");
+        System.out.println("sproyecto de consola.");
     }
 }
---
+++ src/Operaciones.java
@@ -0,0 +1,9 @@
+public class Operaciones {
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+}
