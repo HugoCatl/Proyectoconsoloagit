cadfc978401dc2b75f69923616870061d75707be
HugoCatl <hugocaiba@gmail.com>
2024-12-03 23:09:11 +0100

    Mensaje bienvenida despedida

---
+++ src/Mensajesops.java
@@ -0,0 +1,9 @@
+public class Mensajesops {
+    public static void mostrarBienvenida() {
+        System.out.println("Hola.");
+    }
+
+    public static void mostrarDespedida() {
+        System.out.println("Adios");
+    }
+}
